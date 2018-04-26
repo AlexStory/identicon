73cd860ab3393886d70e883561e1017b25c724c7
Alex Story <astory11@yahoo.com>
2018-04-26 07:34:53 -0500

    building grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -11,16 +11,24 @@ defmodule Identicon do
   end
 
   def build_grid(image) do
+    matrix =
+      image.hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_list/1)
 
+    %Identicon.Image{image | grid: matrix}
   end
 
-  def pick_color(%Identicon.Image{hex: [r,g,b | _]} = image) do
-    %Identicon.Image{image | color: {r, g, b} }
+  defp mirror_list([a,b,c]), do: [a, b, c, b, a]
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
--- lib/image.ex
+++ lib/image.ex
@@ -1,7 +1,7 @@
 defmodule Identicon.Image do
-  defstruct hex: nil, color: nil
+  defstruct hex: nil, color: nil, grid: nil
 
   def get_color(image) do
-    Enum.take image.hex, 3
+    Enum.take(image.hex, 3)
   end
 end
